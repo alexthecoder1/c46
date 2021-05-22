f323311b1fb6b40450db04e1533559ba8e509042
alexthecoder1 <74997835+alexthecoder1@users.noreply.github.com>
2021-05-21 17:17:11 -0700

    Add files via upload

---
+++ The adventure of ron/sketch.js
@@ -0,0 +1,14 @@
+var ron, background, monster, sword, house, table, staff, armour, boots, bowandarrow, chest, boat, flag, torch, dirt, lighthouse, cactus, sand, giantsquid, troll, bridge, castle, king, food, horse, saddle, cave, giant, octopuslegsquidheadcrabclawshugebodysharptailcrystals
+var ronimage
+
+function preload(){
+ronimage = loadImage("Imported piskel.gif")
+} 
+function setup(){
+createCanvas(displayWidth, displayHeight)
+ron = createSprite(120,150)
+} 
+function draw(){
+background("blue") 
+drawSprites()
+}
